a3aefebefc07d9fa4c3f3df68b939084a128ad88
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-04 12:19:07 +0000

    [Add]model/Relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_121028) do
+ActiveRecord::Schema.define(version: 2022_08_04_121653) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -94,6 +94,13 @@ ActiveRecord::Schema.define(version: 2022_08_04_121028) do
     t.string "name", default: "", null: false
     t.text "introduction", default: "", null: false
     t.text "address", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
