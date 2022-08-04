70e003132078864fcbe3070b8b13766f54478961
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-04 05:56:59 +0000

    [Add]views/end_user&admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
+  # 顧客用
+  # URL /end_users/sign_in ...
+  devise_for :end_users,skip: [:passwords], controllers: {
+    registrations: "end_user/registrations",
+    sessions: 'end_user/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
