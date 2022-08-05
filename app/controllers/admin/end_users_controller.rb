11e6a31b4496e18e625762a3cf00c9765e53454a
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-05 06:05:34 +0000

    [Add]controller/admin/end_users

---
+++ app/controllers/admin/end_users_controller.rb
@@ -0,0 +1,10 @@
+class Admin::EndUsersController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+
+  def withdraw
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+
+   namespace :admin do
+    patch '/end_users/withdraw' => 'end_users#withdraw'
+    resources :end_users, only: [:index,:edit,:update]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
