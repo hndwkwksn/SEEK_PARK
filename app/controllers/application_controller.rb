27ec84de0f01637b21c14c5a3863a180422c226e
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-05 11:50:36 +0000

    [Add]admin/ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,21 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    end_user_path(@end_user)
+    case resource
+    when Admin then
+      admin_end_users_path
+    when EndUser then
+      end_user_path(@end_user)
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    case resource
+    when :admin then
+      new_admin_session_path
+    when :end_user then
+      root_path
+    end
   end
 
   protected
