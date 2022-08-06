4e3f45c13300aca85af802bf89aab9d227fb6ff0
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-06 02:36:20 +0000

    [Add]end_user/会員詳細ページ/会員情報&編集ボタン

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "7c2d7d4d83eb446ca8021b0eeb6947fd.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  # Railsでの画像サイズ変更の影響でのエラー回避の為?
+  config.active_job.queue_adapter = :inline
 end
