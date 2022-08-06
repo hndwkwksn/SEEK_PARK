f3e71289a2781825935dcd26e8f6ed64ec640e7a
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-06 04:43:11 +0000

    [Add]end_user/profile_imageコメント追加

--- app/controllers/end_user/end_users_controller.rb
+++ app/controllers/end_user/end_users_controller.rb
@@ -6,9 +6,23 @@ class EndUser::EndUsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @end_user.update(end_user_params)
+      redirect_to end_user_path(@end_user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+
   def unsubscribe
   end
 
   def withdraw
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :introduction, :profile_image)
   end
 end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -6,10 +6,10 @@ class EndUser < ApplicationRecord
 
   has_one_attached :profile_image
 
-  # 画像サイズの変更をする為
+  # 画像サイズの変更をする為　※'resize_to_limit'は元の画像より大きくすることは出来ない！
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
