623ac763f2f8f3617a0ec865cc92ff21f455febc
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-07 11:02:02 +0000

    [Add]favorite機能/view/park/show

--- app/controllers/end_user/favorites_controller.rb
+++ app/controllers/end_user/favorites_controller.rb
@@ -1,14 +1,14 @@
 class EndUser::FavoritesController < ApplicationController
   def create
     park = Park.find(params[:park_id])
-    favorite = current_user.favorites.new(park_id: park.id)
+    favorite = current_end_user.favorites.new(park_id: park.id)
     favorite.save
     redirect_to park_path(park)
   end
 
   def destroy
     park = Park.find(params[:park_id])
-    favorite = current_user.favorites.find_by(park_id: park.id)
+    favorite = current_end_user.favorites.find_by(park_id: park.id)
     favorite.destroy
     redirect_to park_path(park)
   end
