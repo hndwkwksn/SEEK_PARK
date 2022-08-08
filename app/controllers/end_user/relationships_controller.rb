4b611af85f4ea0221e9c54bcc2ec2653bdd2a1d7
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-08 08:23:02 +0000

    [Update]フォロー機能/コントローラー/before_action :set_end_user

--- app/controllers/end_user/relationships_controller.rb
+++ app/controllers/end_user/relationships_controller.rb
@@ -1,29 +1,32 @@
 class EndUser::RelationshipsController < ApplicationController
   before_action :authenticate_end_user!
+  before_action :set_end_user
 
   # フォローするとき
   def create
-    end_user = EndUser.find(params[:end_user_id])
-    current_end_user.follow(end_user)
+    current_end_user.follow(@end_user)
     redirect_to request.referer
   end
 
   # フォロー外すとき
   def destroy
-    end_user = EndUser.find(params[:end_user_id])
-    current_end_user.unfollow(end_user)
+    current_end_user.unfollow(@end_user)
     redirect_to request.referer
   end
 
   # フォロー一覧
   def followings
-    end_user = EndUser.find(params[:end_user_id])
-    @end_users = end_user.followings
+    @end_users = @end_user.followings
   end
 
   # フォロワー一覧
   def followers
-    end_user = EndUser.find(params[:end_user_id])
-    @end_users = end_user.followers
+    @end_users = @end_user.followers
+  end
+
+private
+
+  def set_end_user
+    @end_user = EndUser.find(params[:end_user_id])
   end
 end
