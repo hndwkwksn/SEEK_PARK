da4ac03e8fcc5d8a6b6551d52a9f5089430d1c15
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-09 06:33:55 +0000

    [Update]end_user/会員機能/ssesion_controller/退会していた時のアクション

--- app/controllers/end_user/sessions_controller.rb
+++ app/controllers/end_user/sessions_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class EndUser::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  before_action :end_user_state, only: [:create]
+  before_action :reject_inactive_end_user, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -19,23 +18,23 @@ class EndUser::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  protected
-  # 退会しているかを判断するメソッド
-  def end_user_state
-    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+  # 退会済みの会員が、同じアカウントでログイン出来ないようにするメソッド
+  def reject_inactive_end_user
+    # ログイン時に入力されたemailに対応するユーザーが存在するか探す。
     @end_user = EndUser.find_by(email: params[:end_user][:email])
-    ## アカウントを取得できなかった場合、このメソッドを終了する
-    return if !@end_user
-    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @end_user.valid_password?(params[:end_user][:password])
-      ## 【処理内容3】
+    if @end_user
+      # 入力されたパスワードが正しいことを確認。
+      if @end_user.valid_password?(params[:end_user][:password]) && !@end_user.is_deleted
+        flash[:danger] = 'お客様は退会済みです。申し訳ございませんが、別のメールアドレスをお使いください。'
+        redirect_to new_end_user_session_path
+      end
     end
   end
 end
