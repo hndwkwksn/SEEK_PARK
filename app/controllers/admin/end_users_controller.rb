d5373eb1b65131033769890ed8355c229d1830a4
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-09 06:18:01 +0000

    [Add]end_user/会員機能/ssesion_controller/退会していた時のアクション

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -5,9 +5,13 @@ class Admin::EndUsersController < ApplicationController
 
   def edit
     @end_user = EndUser.find(params[:id])
+    @park = Park.find(params[:id])
   end
 
   def withdraw
     @end_user = EndUser.find(params[:id])
+    # is_deletedカラムをtrueに変更することにより削除フラグを立てる
+    @end_user.update(is_deleted: true)
+    reset_session
   end
 end
