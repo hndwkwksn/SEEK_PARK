8d2e83e642762b8bd95afc0308832ae6ed9fc3e7
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-10 05:35:20 +0000

    [Add]jQuery導入

--- app/controllers/end_user/favorites_controller.rb
+++ app/controllers/end_user/favorites_controller.rb
@@ -2,13 +2,15 @@ class EndUser::FavoritesController < ApplicationController
   before_action :set_park, only: [:create, :destroy]
 
   def create
-    favorite = current_end_user.favorites.new(park_id: park.id)
+    favorite = current_end_user.favorites.new(park_id: @park.id)
     favorite.save
+    render 'replace_btn'
   end
 
   def destroy
-    favorite = current_end_user.favorites.find_by(park_id: park.id)
+    favorite = current_end_user.favorites.find_by(park_id: @park.id)
     favorite.destroy
+    render 'replace_btn'
   end
 
   private
