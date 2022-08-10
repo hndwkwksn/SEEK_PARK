f545f963a074aa8d300f8f0ee09de355683b8f0f
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-10 01:04:16 +0000

    [Update]いいね機能/非同期処理/favorite_controller & view/favorite/_btn

--- app/controllers/end_user/favorites_controller.rb
+++ app/controllers/end_user/favorites_controller.rb
@@ -1,15 +1,19 @@
 class EndUser::FavoritesController < ApplicationController
+  before_action :set_park, only: [:create, :destroy]
+
   def create
-    park = Park.find(params[:park_id])
     favorite = current_end_user.favorites.new(park_id: park.id)
     favorite.save
-    redirect_to park_path(park)
   end
 
   def destroy
-    park = Park.find(params[:park_id])
     favorite = current_end_user.favorites.find_by(park_id: park.id)
     favorite.destroy
-    redirect_to park_path(park)
+  end
+
+  private
+
+  def set_park
+    @park = Park.find(params[:park_id])
   end
 end
