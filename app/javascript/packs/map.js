3f2ed09d9b7e50b8e47d747b02910b056b21ea22
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-11 12:18:41 +0000

    [Add]GoogleMapAPI/画面表示

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,15 +1,40 @@
-// let map
-// let geocoder
 
-// function initMap(){
-//   geocoder = new google.maps.Geocoder()
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     //latが緯度、lngが経度
-//     center: {lat: 40.7828, lng:-73.9653},
-//     zoom: 12,
-//   });
-//   marker = new google.maps.Marker({
-//     position:  {lat: 40.7828, lng:-73.9653},
-//     map: map
-//   });
-// }
+  let map
+
+  function initMap(){
+    geocoder = new google.maps.Geocoder()
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 35.690921, lng:139.700257},
+      zoom: 12,
+    });
+
+    marker = new google.maps.Marker({
+      position:  {lat: 35.690921, lng:139.700257},
+      map: map
+    });
+  }
+
+  let geocoder
+
+  function codeAddress(){
+    let inputAddress = document.getElementById('address').value;
+
+    geocoder.geocode( { 'address': inputAddress}, function(results, status) {
+      if (status == 'OK') {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('該当する結果がありませんでした：' + status);
+      }
+    });
+  }
+
+// ブラウザにinitMapを定義
+window.onload = function() {
+    initMap()
+}
+window.addEventListener("click", codeAddress);
