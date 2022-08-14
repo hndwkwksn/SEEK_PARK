e9d4566ee77407ccabc1c45a5b920a8d206a0df3
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-14 04:18:21 +0000

    [Add]ページネーション/∞ページ実装/jscroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,6 +14,7 @@ import "bootstrap";
 import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all';
 import "./map";
+import "./jquery.jscroll.min";
 
 Rails.start()
 Turbolinks.start()
