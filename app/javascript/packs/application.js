7a29086100e2074d614c4e0c33e412acb14320f4
WakuHonda <105696893+hndwkwksn@users.noreply.github.com>
2022-08-19 07:37:38 +0000

    [Add]top/背景スライドショー

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,8 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all';
-import "skippr.min"
-import "slideshow"
+import "./skippr.min"
+import "./slideshow"
 
 Rails.start()
 Turbolinks.start()
--- app/javascript/packs/slideshow.js
+++ app/javascript/packs/slideshow.js
@@ -1,13 +1,13 @@
 $(document).on('turbolinks:load', function() {
   $(document).ready(function () {
     $("#theTarget").skippr({
-      // スライドショーの変化 ("fade" or "slide")
-      transition : 'slide',
+      // スライドショーの変化
+      transition : 'fade',
       // 変化に係る時間(ミリ秒)
-      speed : 1000,
+      speed : 2000,
       // easingの種類
       easing : 'easeOutQuart',
-      // ナビゲーションの形("block" or "bubble")
+      // ナビゲーションの形
       navType : 'bubble',
       // 子要素の種類('div' or 'img')
       childrenElementType : 'div',
@@ -16,7 +16,7 @@ $(document).on('turbolinks:load', function() {
       // スライドショーの自動再生(falseで自動再生なし)
       autoPlay : true,
       // 自動再生時のスライド切替間隔(ミリ秒)
-      autoPlayDuration : 3000,
+      autoPlayDuration : 7000,
       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
       keyboardOnAlways : true,
       // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
